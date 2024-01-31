80ade01695afe15a8ae3a91689dd4ce27f025409
KingMitch <mitch11223@gmail.com>
2024-01-31 18:29:57 -0500

    Updates 2nd model

--- model_2.py
+++ model_2.py
@@ -3,14 +3,26 @@ import torch
 from torch.utils.data import DataLoader, TensorDataset
 from torch import nn
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
-class NeuralNetwork(nn.Module):
-    def __init__(self, input_size, output_size=1):
-        super().__init__()
+#turn offense and defense dfs into numbers/tensors
+#choose appropriate neural network for tensors
+#leanrs represnetation/patterns/features/weights
+#outputs numbers
+#turn into human readable fmt
 
 
-    def forward(self, x):
 
 
+player_name = 'Damian Lillard'
+player_team = 'MIL'
+opposing_team = 'POR'
+offense_cols = ['Opponent Height','Opponent Weight','Avg Min'] #matchup mins here?
+
+offense = pd.read_csv(f'players/matchups/data/offense/{player_name}_matchups.csv')
+defense = pd.read_csv(f'players/matchups/data/defense/{player_name}_matchups.csv')
+
+
+print(offense)
